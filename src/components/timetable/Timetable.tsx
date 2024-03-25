cf19748eda8e9fd895c68f6af2a2b98e1e742bbb
dreamfulbud <dreamfulbud@naver.com>
2024-03-25 13:45:35 +0900

    📝 Add: 프로그램 추가 및 CSS 수정

--- src/components/timetable/Timetable.tsx
+++ src/components/timetable/Timetable.tsx
@@ -3,13 +3,17 @@ import TimetableItem from "./TimetableItem";
 
 export default function Timetable({ data, day }: { data: any; day: string }) {
   return (
-    <div className={styles.fadeIn}>
+    <div className={`${styles.fadeIn}`}>
       <h2 className={styles.tit}>
         {day} <span>2024.04.{day === "Day1" ? "12(금)" : "13(토)"}</span>
       </h2>
-      <ul className={styles.section_cont}>
+      <ul
+        className={`${styles.section_cont}  ${
+          day === "Day2" ? styles.column2 : styles.column3
+        }`}
+      >
         {data.map((value: any, index: number) => (
-          <TimetableItem key={index} item={value} index={index} />
+          <TimetableItem key={index} item={value} index={index} day={day} />
         ))}
       </ul>
     </div>
--- src/components/timetable/TimetableItem.tsx
+++ src/components/timetable/TimetableItem.tsx
@@ -3,22 +3,47 @@ import styles from "./Timetable.module.scss";
 export default function TimetableItem({
   item,
   index,
+  day,
 }: {
   item: any;
+  day: string;
   index: number;
 }) {
   const row = item.times > 1 ? `${index - 2}/${index - 2 + item.times}` : null;
 
+  let grid_Column;
+  if (item.times === 0) {
+    grid_Column = `1 / span 6`;
+  } else {
+    if (item.space === "트랙룸") {
+      if (index === 9) {
+        grid_Column = `1 / span 2`;
+      } else {
+        grid_Column = `1 / span 3`;
+      }
+    } else if (item.space === "펀치룸") {
+      if (index === 10) {
+        grid_Column = `3 / span 2`;
+      } else {
+        grid_Column = `4 / span 3`;
+      }
+    } else {
+      grid_Column = `5 / span 2`;
+    }
+  }
+
   return (
     <li
-      style={row ? { gridRow: row } : {}}
+      style={{ gridColumn: grid_Column, gridRow: `auto / span ${item.times}` }}
       className={item.times === 0 ? styles.col2 : ""}
     >
       <div className={styles.spaceTime}>
         <p
           className={`${styles.space} ${
             item.space === "펀치룸" && styles.space2
-          } ${item.space === "로비" && styles.space3}`}
+          } ${item.space === "로비" && styles.space3} ${
+            item.space === "커뮤니케이션룸" && styles.space4
+          }`}
         >
           {item.space}
         </p>
