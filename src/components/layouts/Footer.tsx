6c0372c78c7365561101b9efced07d8c97f2ae3b
dreamfulbud <dreamfulbud@naver.com>
2024-03-28 09:45:51 +0900

    📝 Add: reflow 로고 추가, 텍스트 분할

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
 import styles from "./Footer.module.scss";
-
+import data from "@/data/data.json";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>
-        주관: 제주지역혁신플랫폼(RIS) 지능형서비스사업단,
-        (재)제주창조경제혁신센터, GDG Jeju, (주)위니브 | 후원/협찬: 리플로우, 제주 한라수
-      </p>
+      <p>{data.footer}</p>
       <div className={styles.logos}>
         <Image
           src="/images/RIS.svg"
@@ -35,6 +32,13 @@ const Footer = () => {
           alt=""
           width={144}
           height={30}
+          className={styles.weniv}
+        />
+        <Image
+          src="/images/reflow.svg"
+          alt=""
+          width={126}
+          height={40}
           className={styles.weniv}
         />
       </div>
